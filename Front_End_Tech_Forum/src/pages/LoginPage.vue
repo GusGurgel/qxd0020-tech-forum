adf166193cb6556466092bb90ae1cc737b34565f
GusGurgel <gurgelg15@gmail.com>
2024-09-04 11:10:26 -0300

    Iniciando projeto de backend e adicionando mais icones na tela de login

--- Front_End_Tech_Forum/src/pages/LoginPage.vue
+++ Front_End_Tech_Forum/src/pages/LoginPage.vue
@@ -1,6 +1,6 @@
 <script setup>
 
-import { PhUserCircle } from '@phosphor-icons/vue';
+import { PhUserCircle, PhEnvelope, PhKey } from '@phosphor-icons/vue';
 import { ref } from 'vue';
 
 const email = ref('')
@@ -16,34 +16,38 @@ function haddleSubmit(e) {
 
 <template>
   <main class="mt-5">
-    <div class="container w-25 border  rounded  p-4">
-      <div class="row">
-        <div class="col-12">
-          <div class="p-3 mb-3 d-flex align-items-center justify-content-center">
-            <PhUserCircle :size="32" class="me-2" />
-            <div class="lexend-font">
-              Login
+    <div class="row d-lg-flex justify-content-center">
+      <div class="border rounded p-3 col-lg-3">
+        <div class="row">
+          <div class="col-sm-12">
+            <div class="border-bottom border-dark p-3 mb-3 d-flex align-items-center justify-content-center">
+              <PhUserCircle :size="32" class="me-2" />
+              <div class="lexend-font">
+                Login
+              </div>
             </div>
           </div>
-        </div>
-        <div class="col-12">
-          <form class="w-100" action="/login" method="POST" @submit="haddleSubmit">
-            <div class="form-group mb-3">
-              <label for="email">Email address</label>
-              <input type="email" name="email" v-model="email" class="form-control" id="email"
-                aria-describedby="emailHelp" placeholder="Enter email" required>
-            </div>
-            <div class="form-group">
-              <label for="password">Password</label>
-              <input type="password" name="password" v-model="password" class="form-control" id="password"
-                placeholder="Password" required>
-            </div>
-            <small class="text-danger">
-            </small>
-            <div class="text-center pt-4">
-              <button type="submit" class="btn btn-dark">Enter</button>
-            </div>
-          </form>
+          <div class="col-12">
+            <form class="w-100" action="/login" method="POST" @submit="haddleSubmit">
+              <div class="form-group mb-3">
+                <PhEnvelope :size="20" class="me-2" />
+                <label for="email" class="pb-2">Email</label>
+                <input type="email" name="email" v-model="email" class="form-control" id="email"
+                  aria-describedby="emailHelp" placeholder="Enter email" required>
+              </div>
+              <div class="form-group">
+                <PhKey :size="20" class="me-2" />
+                <label for="password" class="pb-1">Password</label>
+                <input type="password" name="password" v-model="password" class="form-control" id="password"
+                  placeholder="Password" required>
+              </div>
+              <small class="text-danger">
+              </small>
+              <div class="text-center pt-4">
+                <button type="submit" class="btn btn-dark pe-4 ps-4">Enter</button>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
     </div>
