8568e2294a3c943d506387db4f48912cf422b8e8
GusGurgel <gurgelg15@gmail.com>
2024-09-17 11:07:59 -0300

    Alterando sistema de pastas. Removendo pasta Tests e criando pasta de requests para o backend

--- Back_End_Tech_Forum/types/generated/contentTypes.d.ts
+++ Back_End_Tech_Forum/types/generated/contentTypes.d.ts
@@ -764,7 +764,6 @@ export interface PluginUsersPermissionsUser extends Schema.CollectionType {
   };
   options: {
     draftAndPublish: false;
-    timestamps: true;
   };
   attributes: {
     username: Attribute.String &
@@ -793,6 +792,7 @@ export interface PluginUsersPermissionsUser extends Schema.CollectionType {
       'manyToOne',
       'plugin::users-permissions.role'
     >;
+    image: Attribute.Media<'images'>;
     createdAt: Attribute.DateTime;
     updatedAt: Attribute.DateTime;
     createdBy: Attribute.Relation<
